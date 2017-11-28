bd8d021aa7243c6b5bacd379f223624dff9a083d
Vlad <sergayen@post.bgu.ac.il>
2017-11-28 12:14:35 +0200

    YOU 32 BIT SCRUB DELETE SYSTEM32

--- src/Test/Main.java
+++ src/Test/Main.java
@@ -8,6 +8,9 @@ import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
+//        long hz= Runtime.getRuntime().totalMemory();
+//        System.out.println(hz);
+
         long startTime = System.currentTimeMillis();
         ReadFile read = new ReadFile();
         Parser parse = new Parse();
